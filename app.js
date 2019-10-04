0e635a36c75a64f695f106ff68f11c6837a91015
Yuliia Kandych <yuliiakandych@Yuliia-DIPLY-0138-19992.local>
2019-10-04 00:32:53 -0400

    adding hbs files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const port = process.env.PORT || 3000;
 
@@ -7,10 +8,15 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
 app.get('/', (req, res) => {
   console.log('at the home route');
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+  res.render("home");
 })
+
+
 
 
 
